f21f5dbd639b967ff21ba59c96db6b913821e653
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-12-27 10:20:53 +0700

    test crisp implement

---
+++ plugins/crisp.js
@@ -0,0 +1,13 @@
+export default () => {
+    window.$crisp = [];
+    window.CRISP_WEBSITE_ID = "8ea69bdb-5548-48fb-8a5d-ae9d79378ffa";
+  
+    (function() {
+      var d = document;
+      var s = d.createElement("script");
+  
+      s.src = "https://client.crisp.chat/l.js";
+      s.async = 1;
+      d.getElementsByTagName("head")[0].appendChild(s);
+    })();
+  };
