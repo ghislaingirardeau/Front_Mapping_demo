ce4355ef23180f2a2dc50c06232f263ec8807260
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2022-03-12 16:00:39 +0700

    icons from cvs

--- pages/index.vue
+++ pages/index.vue
@@ -102,6 +102,10 @@ export default {
           type: element.type,
           coordinates: coordinates,
         },
+        icon: {
+          type: element.icon,
+          color: element.color
+        }
       };
       layer.push(newGeoJson);
     };
@@ -251,67 +255,10 @@ export default {
       }
 
       // FUNCTION RETURN ICON HOUSE SVG DEPENDING ON COLOR PARAMS
-      function createIcon(category, subCategory, size) {
-        let colors;
-        let path;
-        let view = "0 0 24 24";
-        if (category === "house") {
-          switch (subCategory) {
-            case "not interview":
-              colors = "black";
-              break;
-            case "indebted":
-              colors = "orange";
-              break;
-            case "interview":
-              colors = "green";
-              break;
-            case "lost land":
-              colors = "red";
-              break;
-          }
-        }
-        if (category === "well") {
-          switch (subCategory) {
-            case "private":
-              colors = "blue";
-              break;
-            case "public":
-              colors = "red";
-              break;
-          }
-        }
-        switch (
-          category // ICON DEPENDING ON THE CATEGORY
-        ) {
-          case "house":
-            path = `<path fill="${colors}" d="M10,20V14H14V20H19V12H22L12,3L2,12H5V20H10Z" />`;
-            break;
-          case "bath":
-            path = `<path d="M12,3.77L11.25,4.61C11.25,4.61 9.97,6.06 8.68,7.94C7.39,9.82 6,12.07 6,14.23A6,6 0 0,0 12,20.23A6,6 0 0,0 18,14.23C18,12.07 16.61,9.82 15.32,7.94C14.03,6.06 12.75,4.61 12.75,4.61L12,3.77M12,6.9C12.44,7.42 12.84,7.85 13.68,9.07C14.89,10.83 16,13.07 16,14.23C16,16.45 14.22,18.23 12,18.23C9.78,18.23 8,16.45 8,14.23C8,13.07 9.11,10.83 10.32,9.07C11.16,7.85 11.56,7.42 12,6.9Z"/>`;
-            break;
-          case "grave":
-            path = `<path fill="black" d="M10,2H14C17.31,2 19,4.69 19,8V18.66C16.88,17.63 15.07,17 12,17C8.93,17 7.12,17.63 5,18.66V8C5,4.69 6.69,2 10,2M8,8V9.5H16V8H8M9,12V13.5H15V12H9M3,22V21.31C5.66,19.62 13.23,15.84 21,21.25V22H3Z" />`;
-            break;
-          case "chief House":
-            path = `<path fill="black" d="M10,2V4.26L12,5.59V4H22V19H17V21H24V2H10M7.5,5L0,10V21H15V10L7.5,5M14,6V6.93L15.61,8H16V6H14M18,6V8H20V6H18M7.5,7.5L13,11V19H10V13H5V19H2V11L7.5,7.5M18,10V12H20V10H18M18,14V16H20V14H18Z" />`;
-            break;
-          case "homestay":
-            path = `<path fill="black" d="M12,3L2,12H5V20H19V12H22L12,3M12,7.7C14.1,7.7 15.8,9.4 15.8,11.5C15.8,14.5 12,18 12,18C12,18 8.2,14.5 8.2,11.5C8.2,9.4 9.9,7.7 12,7.7M12,10A1.5,1.5 0 0,0 10.5,11.5A1.5,1.5 0 0,0 12,13A1.5,1.5 0 0,0 13.5,11.5A1.5,1.5 0 0,0 12,10Z" />`;
-            break;
-          case "communal":
-            path = `<path fill="black" d="M12 5C6.5 5 2 9.5 2 15V21H22V15C22 9.5 17.5 5 12 5M12 7C14.53 7 16.78 8.17 18.25 10H5.76C7.22 8.17 9.47 7 12 7M8 19H4V15C4 13.94 4.21 12.93 4.58 12H8V19M14 19H10V12H14V19M20 19H16V12H19.42C19.79 12.93 20 13.94 20 15V19Z" />`;
-            break;
-          case "well":
-            path = `<path fill="${colors}" d="M3.62 8H5V15H7V8H11V10H13V8H17V15H19V8H20.61C21.16 8 21.61 7.56 21.61 7C21.61 6.89 21.6 6.78 21.56 6.68L19 2H5L2.72 6.55C2.47 7.04 2.67 7.64 3.16 7.89C3.31 7.96 3.46 8 3.62 8M6.24 4H17.76L18.76 6H5.24L6.24 4M2 16V18H4V22H20V18H22V16H2M18 20H6V18H18V20M13.93 11C14.21 11 14.43 11.22 14.43 11.5C14.43 11.5 14.43 11.54 14.43 11.56L14.05 14.56C14 14.81 13.81 15 13.56 15H10.44C10.19 15 10 14.81 9.95 14.56L9.57 11.56C9.54 11.29 9.73 11.04 10 11C10.03 11 10.05 11 10.07 11H13.93Z" />`;
-            break;
-          case "worship":
-            path = `<path fill="black" d="M15.59,9C17.7,7.74 19,5.46 19,3H17A5,5 0 0,1 12,8A5,5 0 0,1 7,3H5C5,5.46 6.3,7.74 8.41,9C5.09,11 4,15.28 6,18.6C7.97,21.92 12.27,23 15.59,21C18.91,19.04 20,14.74 18,11.42C17.42,10.43 16.58,9.59 15.59,9M12,20A5,5 0 0,1 7,15A5,5 0 0,1 12,10A5,5 0 0,1 17,15A5,5 0 0,1 12,20Z" />`;
-            break;
-        }
+      function createIcon(type, color, size) {
         return L.divIcon({
-          className: `${category}-icon`,
-          html: `<svg style="width:${size}px;height:${size}px" viewBox="${view}">${path}</svg>`,
+          className: `${type}-icon`,
+          html: `<i aria-hidden="true" class="v-icon notranslate mdi mdi-${type} theme--dark" style="font-size: ${size}; color:${color};"></i>`,
         });
       }
 
@@ -338,9 +285,9 @@ export default {
           // CREATE THE MARKERS
           return L.marker(latlng, {
             icon: createIcon(
-              feature.properties.category,
-              feature.properties.subCategory,
-              "16"
+              feature.icon.type,
+              feature.icon.color,
+              "small"
             ),
           });
         },
--- pages/test.vue
+++ pages/test.vue
@@ -1,8 +1,10 @@
 <template>
     <div>
-        <div v-for="item in items" :key="item.id">
-            {{ item }}
-        </div>
+        <h1>test</h1>
+        <p>je choisis un point ou une area</p>
+        <p>je lui donne un nom 'geotype'</p>
+        <p>si sous catégorie = array, vide si pas de sous category</p>
+        <p>je lui attribut une icone + une couleur</p>
     
     </div>
 </template>
