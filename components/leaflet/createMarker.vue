78c178f9250f4562ab64da616df0df4dd4776c34
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2022-05-03 12:15:10 +0700

    clean code

--- components/leaflet/createMarker.vue
+++ components/leaflet/createMarker.vue
@@ -241,23 +241,6 @@ export default {
       valid: true,
       validSub: true,
       iterateOfColorArray: 0,
-      swatches: [
-        '#FF0000',
-        '#AA0000',
-        '#550000',
-        '#FFFF00',
-        '#AAAA00',
-        '#555500',
-        '#00FF00',
-        '#00AA00',
-        '#005500',
-        '#00FFFF',
-        '#00AAAA',
-        '#005555',
-        '#0000FF',
-        '#0000AA',
-        '#000055',
-      ],
       // item for text fields
       subCategory: {
         selected: '',
@@ -267,7 +250,7 @@ export default {
       colorSelected: '',
       newIcon: {
         type: 'Point',
-        category: 'sac',
+        category: 'home',
         subCategory: [],
         icon: 'home',
         color: [],
