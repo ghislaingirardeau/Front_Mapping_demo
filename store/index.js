acd9809148fc80e23cda484629928956e7b5574f
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2022-05-11 13:06:04 +0700

    remove indeexDB fct

--- store/index.js
+++ store/index.js
@@ -90,56 +90,6 @@ export const actions = {
             commit('ERROR_REPONSE', error.message)
         }
     },
-    // INDEXEDDB
-    /* loadMarkers(context) {
-        let results = []
-        const requestIndexedDB = window.indexedDB.open('Map_Database', 1)
-        requestIndexedDB.onerror = (event) => {
-            console.log(event)
-        }
-        requestIndexedDB.onsuccess = (event) => {
-            let db = event.target.result
-
-            let transaction = db.transaction('markers', 'readwrite')
-            let store = transaction.objectStore('markers') // store = table in sql
-            let idQuery = store.openCursor() // recherche sur l'id
-            idQuery.onsuccess = (event) => {
-                var cursor = event.target.result
-
-                if (cursor) {
-                    // if a get an array of sub category, i create a new object to send in this.markers array
-                    if (cursor.value.subCategory.length > 0) {
-                        for (
-                            let index = 0;
-                            index < cursor.value.subCategory.length;
-                            index++
-                        ) {
-                            let multiMarker = {
-                                type: cursor.value.type,
-                                category: cursor.value.category,
-                                subCategory: [],
-                                icon: cursor.value.icon,
-                                color: [],
-                            }
-                            multiMarker.subCategory.push(cursor.value.subCategory[index])
-                            multiMarker.color.push(cursor.value.color[index])
-                            results.push(multiMarker)
-                        }
-                    } else {
-                        results.push(cursor.value)
-                    }
-                    cursor.continue()
-                } else {
-                    context.commit('SAVE_MARKERS', results);
-                    console.log('No more entries!')
-
-                }
-            }
-            transaction.oncomplete = () => {
-                db.close()
-            }
-        }
-    }, */
     markersOnCreate({ commit, state }, newMarker) {
         let flatMarkers = []
         if (newMarker.subCategory.length > 0) {
