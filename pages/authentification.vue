c49b343718da73bf56e4740c42a69469edd3b938
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2022-05-17 16:23:05 +0700

    make the display

--- pages/authentification.vue
+++ pages/authentification.vue
@@ -1,42 +1,39 @@
 <template>
-  <v-container>
-    <v-row>
-      <theNavBar/>
-      <v-col cols="11" sm="6">
-        <v-overlay :value="overlay">
-          <v-progress-circular indeterminate size="64"></v-progress-circular>
-        </v-overlay>
+  <v-row class="text-center" justify="space-around">
+    <theNavBar />
+    <v-col cols="11">
+      <h1>My account</h1>
+    </v-col>
+    <v-col cols="11" sm="6">
+      <v-overlay :value="overlay">
+        <v-progress-circular indeterminate size="64"></v-progress-circular>
+      </v-overlay>
 
-        <authForm
-          :login="signType"
-          :doThis="signType ? loginUser : createUser"
-        />
-        <p v-if="errorMessage">{{ errorMessage }}</p>
-        <v-btn
-          v-if="userAuth"
-          class="mb-3"
-          color="teal"
-          outlined
-          @click="signOut(false)"
-          >Log Out</v-btn
-        >
+      <authForm :login="signType" :doThis="signType ? loginUser : createUser" />
+      <p v-if="errorMessage" class="info__message--alert">{{ errorMessage }}</p>
+      <span class="info__message">You forgot your password ? </span>
+      <v-btn small text color="teal" @click="forgotPassword">reset here</v-btn>
+      <p v-if="resetMessage" class="info__message--success">{{ resetMessage }}</p>
+      <v-btn
+        v-if="userAuth"
+        class="mb-3"
+        color="teal"
+        outlined
+        @click="signOut(false)"
+        >Log Out</v-btn
+      >
 
-        <p class="mt-3">
-          {{
-            signType
-              ? "If you don't have connection ID ? Please click 'Sign Up'"
-              : 'Back to the login'
-          }}
-        </p>
-        <v-btn
-          class="mb-3"
-          color="teal"
-          outlined
-          @click="signType = !signType"
-          >{{ signType ? 'Sign Up' : 'Login' }}</v-btn
-        >
-      </v-col>
-    </v-row>
+      <p class="mt-3">
+        {{
+          signType
+            ? "If you don't have connection ID ? Please click 'Sign Up'"
+            : 'Back to the login'
+        }}
+      </p>
+      <v-btn class="mb-3" color="teal" outlined @click="signType = !signType">{{
+        signType ? 'Sign Up' : 'Login'
+      }}</v-btn>
+    </v-col>
     <!-- <v-btn @click="postRealTimeDB"> write DB </v-btn>
     <v-btn @click="getRealTimeDB"> read DB </v-btn>
     <v-btn @click="updateRealTimeDB"> update DB </v-btn>
@@ -45,7 +42,7 @@
     <v-btn @click="tokenLog"> log by token </v-btn>
 
     <h2 v-if="userAuth">The userAuth log : {{ userAuth.displayName }}</h2> -->
-  </v-container>
+  </v-row>
 </template>
 
 <script>
@@ -57,6 +54,7 @@ export default {
   data() {
     return {
       overlay: false,
+      resetMessage: undefined,
       RLvalue: undefined,
       signType: true,
       id: 'ID123444',
@@ -76,12 +74,23 @@ export default {
       this.overlay = true
       await this.login(data).then(() => {
         this.overlay = false
-        /* this.$router.push('/myData') */
+        this.$router.push('/myData')
       })
     },
-    /* signOut() {
-      this.$fire.auth.signOut()
-    }, */
+    async forgotPassword() {
+      await this.$fire.auth
+        .sendPasswordResetEmail(this.userAuth.email)
+        .then(() => {
+          console.log('Email to reset the password send')
+          this.resetMessage =
+            'An email sent, follow the link to reset your password !'
+        })
+        .catch((error) => {
+          console.log(error, 'fail to send the email')
+          this.resetMessage = 'Fail to send the email !'
+        })
+    },
+
     // NUXT FIREBASE REALTIME DB
     async getRealTimeDB() {
       console.log(this.$fire.auth.currentUser)
@@ -154,16 +163,6 @@ export default {
         return
       }
     },
-    async forgotPassword() {
-      await this.$fire.auth
-        .sendPasswordResetEmail(this.userAuth.email)
-        .then(() => {
-          console.log('Email to reset the password send')
-        })
-        .catch((error) => {
-          console.log(error, 'fail to send the email')
-        })
-    },
     async tokenLog() {
       /* const token = sessionStorage.getItem('TokenGeoMap')
       console.log(token);
@@ -177,8 +176,20 @@ export default {
       console.log(this.$fire.auth)
     },
   },
+  mounted () {
+    this.resetMessage = undefined
+  },
 }
 </script>
 
 <style lang="scss" scoped>
+.info__message {
+  font-size: 0.8rem;
+  &--alert {
+    color: rgb(245, 105, 105);
+  }
+  &--success {
+    color: rgb(105, 245, 168);
+  }
+}
 </style>
