a3c49f6e0d1c8ebe07100887520165520831f711
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2022-05-24 16:06:12 +0700

    debug width

--- components/leaflet/legendModal.vue
+++ components/leaflet/legendModal.vue
@@ -1,54 +1,55 @@
 <template>
-  <v-col
-    :cols="themeDarkColor ? '6' : undefined"
-    :class="themeDarkColor ? 'legend__bloc' : ''"
-  >
-    <v-row class="text-center" v-if="markers.length > 0">
-      <v-col v-for="(i, l) in markers" :key="l" order="first">
-        <span>{{ i.category }}</span>
-        <br />
-        <v-icon v-if="i.icon.length > 0" :color="i.color">
-          mdi-{{ i.icon }}
-        </v-icon>
-        <v-chip
-          :color="i.color"
-          small
-          label
-          outlined
-          v-else-if="i.type === 'Polygon'"
-          >area</v-chip
-        >
-        <v-icon v-else :color="i.color"> mdi-vector-polyline </v-icon>
-        <br />
-        <span v-if="i.subCategory && i.subCategory.length > 0">{{ i.subCategory }}</span>
-      </v-col>
-      <v-col cols="12" v-show="!showPrintMap">
-        <v-slider v-model="size" max="42" min="10" step="4" label="Size" @change="newSize"></v-slider>
-      </v-col>
-    </v-row>
-    <v-row v-else>
-      <v-col>
-        <p>You have to create a marker first !</p>
-      </v-col>
-    </v-row>
-  </v-col>
+  <v-row class="text-center" v-if="markers.length > 0">
+    <v-col v-for="(i, l) in markers" :key="l" order="first">
+      <span>{{ i.category }}</span>
+      <br />
+      <v-icon v-if="i.icon.length > 0" :color="i.color">
+        mdi-{{ i.icon }}
+      </v-icon>
+      <v-chip
+        :color="i.color"
+        small
+        label
+        outlined
+        v-else-if="i.type === 'Polygon'"
+        >area</v-chip
+      >
+      <v-icon v-else :color="i.color"> mdi-vector-polyline </v-icon>
+      <br />
+      <span v-if="i.subCategory && i.subCategory.length > 0">{{
+        i.subCategory
+      }}</span>
+    </v-col>
+    <v-col cols="12" v-show="!showPrintMap">
+      <v-slider
+        v-model="size"
+        max="42"
+        min="10"
+        step="4"
+        label="Size"
+        @change="newSize"
+      ></v-slider>
+    </v-col>
+  </v-row>
+  <v-row v-else>
+    <v-col>
+      <p>You have to create a marker first !</p>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-
 export default {
   data: () => ({
     showContent: false,
-    size: 20
+    size: 20,
   }),
   props: {
     showLegend: Boolean,
-    themeDarkColor: Boolean,
     showPrintMap: Boolean,
-    markers: Array
+    markers: Array,
   },
-  computed: {
-  },
+  computed: {},
   methods: {
     newSize() {
       let elt = document.getElementsByClassName('icon__layer--update--size')
@@ -60,15 +61,11 @@ export default {
       this.showContent = !this.showContent
     },
   },
-  mounted() {
-  },
+  mounted() {},
 }
 </script>
 
 <style lang="scss">
-.legend__bloc {
-  border: 2px solid grey;
-}
 .items--font {
   font-size: 12px;
 }
