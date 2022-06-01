514a250455c5bafa591df4d75320d3bdc9544264
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2022-06-01 15:29:41 +0700

    change modal to bottom sheet

--- components/edit/editMarker.vue
+++ components/edit/editMarker.vue
@@ -1,97 +1,105 @@
 <template>
-  <modalCustom :showModal="showModal" @send-modal="modalResponse">
-    <template v-slot:title> Edit Item </template>
-    <template v-slot:content>
-      <v-form ref="form" v-model="valid" lazy-validation>
-        <p v-if="oldItem.subCategory" class="subCategory__message">
-          This marker is a part of subCategory. You can only change the
-          subcategory name & color !
-        </p>
-        <v-text-field
-          v-model="editItem.category"
-          v-if="!oldItem.subCategory"
-          label="Category name"
-          :rules="rulesEditCat"
-        >
-        </v-text-field>
-        <v-text-field
-          v-model="editItem.icon"
-          v-if="oldItem.icon && !oldItem.subCategory"
-          label="Icon name"
-          hint="Type the first letter to see the icon's list"
-          persistent-hint
-          prefix="mdi-"
-          class="mb-3"
-          :rules="checkIcon"
-          required
-          @keyup="showIconsList(editItem.icon)"
-        >
-        </v-text-field>
-        <v-icon
-          class="ml-3"
-          :color="editItem.color"
-          large
-          v-if="oldItem.icon && !oldItem.subCategory"
-        >
-          mdi-{{ editItem.icon }}
-        </v-icon>
-
-        <v-dialog v-model="dialog" fullscreen hide-overlay>
-          <template v-slot:activator="{ on, attrs }">
-            <span v-show="oldItem.icon.length > 0 && !oldItem.subCategory"
-              >{{ iconSuggestList.length }} icons found</span
+  <v-row justify="center" v-if="showModal">
+    <v-bottom-sheet v-model="showModal" inset persistent>
+      <v-card class="modal__content">
+        <v-card-title>
+          Edit Item
+          <v-spacer></v-spacer>
+          <span @click="modalResponse" class="modal__content--close">&times;</span>
+        </v-card-title>
+        <v-card-text>
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <p v-if="oldItem.subCategory" class="subCategory__message">
+              This marker is a part of subCategory. You can only change the
+              subcategory name & color !
+            </p>
+            <v-text-field
+              v-model="editItem.category"
+              v-if="!oldItem.subCategory"
+              label="Category name"
+              :rules="rulesEditCat"
             >
+            </v-text-field>
+            <v-text-field
+              v-model="editItem.icon"
+              v-if="oldItem.icon && !oldItem.subCategory"
+              label="Icon name"
+              hint="Type the first letter to see the icon's list"
+              persistent-hint
+              prefix="mdi-"
+              class="mb-3"
+              :rules="checkIcon"
+              required
+              @keyup="showIconsList(editItem.icon)"
+            >
+            </v-text-field>
             <v-icon
-              color="primary"
-              v-bind="attrs"
-              v-on="on"
-              v-show="oldItem.icon.length > 0 && !oldItem.subCategory"
-              class="mr-3 iconAddColor"
-              size="24px"
-              >mdi-eye-settings-outline</v-icon
+              class="ml-3"
+              :color="editItem.color"
+              large
+              v-if="oldItem.icon && !oldItem.subCategory"
             >
-          </template>
-          <v-card>
-            <v-card-title> Click to select icon </v-card-title>
-            <v-card-text class="ma-2">
-              <v-icon
-                class="ma-2"
-                v-for="(icon, i) in iconSuggestList"
-                :key="i"
-                x-large
-                @click="pickIcon(icon)"
-                >mdi-{{ icon }}</v-icon
-              >
-            </v-card-text>
-            <v-divider></v-divider>
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="primary" outlined @click="dialog = false">
-                Close
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>
-        <v-text-field
-          v-model="editItem.subCategory"
-          v-if="oldItem.subCategory"
-          label="Change SubCategory"
-          :rules="rulesEditSub"
-        >
-        </v-text-field>
+              mdi-{{ editItem.icon }}
+            </v-icon>
 
-        <v-spacer></v-spacer>
-        <v-color-picker
-          v-model="editItem.color"
-          dot-size="25"
-          hide-inputs
-          class="mt-3"
-        ></v-color-picker>
-        <v-spacer></v-spacer>
-        <v-btn color="teal" outlined @click="updateItem"> Save </v-btn>
-      </v-form>
-    </template>
-  </modalCustom>
+            <v-dialog v-model="dialog" fullscreen hide-overlay>
+              <template v-slot:activator="{ on, attrs }">
+                <span v-show="oldItem.icon.length > 0 && !oldItem.subCategory"
+                  >{{ iconSuggestList.length }} icons found</span
+                >
+                <v-icon
+                  color="primary"
+                  v-bind="attrs"
+                  v-on="on"
+                  v-show="oldItem.icon.length > 0 && !oldItem.subCategory"
+                  class="mr-3 iconAddColor"
+                  size="24px"
+                  >mdi-eye-settings-outline</v-icon
+                >
+              </template>
+              <v-card>
+                <v-card-title> Click to select icon </v-card-title>
+                <v-card-text class="ma-2">
+                  <v-icon
+                    class="ma-2"
+                    v-for="(icon, i) in iconSuggestList"
+                    :key="i"
+                    x-large
+                    @click="pickIcon(icon)"
+                    >mdi-{{ icon }}</v-icon
+                  >
+                </v-card-text>
+                <v-divider></v-divider>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="primary" outlined @click="dialog = false">
+                    Close
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+            <v-text-field
+              v-model="editItem.subCategory"
+              v-if="oldItem.subCategory"
+              label="Change SubCategory"
+              :rules="rulesEditSub"
+            >
+            </v-text-field>
+
+            <v-spacer></v-spacer>
+            <v-color-picker
+              v-model="editItem.color"
+              dot-size="25"
+              hide-inputs
+              class="mt-3"
+            ></v-color-picker>
+            <v-spacer></v-spacer>
+            <v-btn color="teal" outlined @click="updateItem"> Save </v-btn>
+          </v-form>
+        </v-card-text>
+      </v-card>
+    </v-bottom-sheet>
+  </v-row>
 </template>
 
 <script>
