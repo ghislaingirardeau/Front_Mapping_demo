179b6c4a6c79aa0d8462a40be6565d374d78c668
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2022-06-25 16:33:09 +0700

    animate form on change log

--- components/authForm.vue
+++ components/authForm.vue
@@ -1,51 +1,63 @@
 <template>
   <v-col cols="11" sm="6">
-    <v-overlay :value="overlay" >
+    <v-overlay :value="overlay">
       <loader-map />
     </v-overlay>
+    <transition name="fade-slideX" mode="out-in">
+      <div :key="signType">
+        <v-form ref="form" v-model="valid" lazy-validation>
+          <v-text-field
+            v-if="!signType"
+            v-model="formData.displayName"
+            label="name"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="formData.email"
+            :rules="emailRules"
+            label="Email"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="formData.password"
+            :rules="passwordRules"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="showPassword ? 'text' : 'password'"
+            @click:append="showPassword = !showPassword"
+            label="Password"
+            required
+          ></v-text-field>
 
-    <v-form ref="form" v-model="valid" lazy-validation>
-      <v-text-field
-        v-if="!signType"
-        v-model="formData.displayName"
-        label="name"
-        required
-      ></v-text-field>
-      <v-text-field
-        v-model="formData.email"
-        :rules="emailRules"
-        label="Email"
-        required
-      ></v-text-field>
-      <v-text-field
-        v-model="formData.password"
-        :rules="passwordRules"
-        :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-        :type="showPassword ? 'text' : 'password'"
-        @click:append="showPassword = !showPassword"
-        label="Password"
-        required
-      ></v-text-field>
-
-      <v-btn color="primary" @click="sendDataForm">
-        {{ signType ? 'Login' : 'Register' }}
-      </v-btn>
-    </v-form>
-    <span class="info__message">You forgot your password ? </span>
-    <v-btn small text color="primary" @click="forgotPassword">reset here</v-btn>
-    <p v-if="resetMessage" class="info__message--success">
-      {{ resetMessage }}
-    </p>
-    <p>
-      {{
-        signType
-          ? "If you don't have connection ID ? Please click 'Sign Up'"
-          : 'Back to the login'
-      }}
-    </p>
-    <v-btn class="mb-3" color="primary" outlined @click="signType = !signType">{{
-      signType ? 'Sign Up' : 'Login'
-    }}</v-btn>
+          <v-btn color="primary" @click="sendDataForm">
+            {{ signType ? 'Login' : 'Register' }}
+          </v-btn>
+        </v-form>
+        <div v-if="signType">
+          <span class="info__message">You forgot your password ? </span>
+          <v-btn small text color="secondary" @click="forgotPassword"
+            >reset here</v-btn
+          >
+        </div>
+        <p v-if="resetMessage" class="info__message--success">
+          {{ resetMessage }}
+        </p>
+        <p>
+          {{
+            signType
+              ? "If you don't have connection ID ? Please click 'Sign Up'"
+              : 'Back to the login'
+          }}
+        </p>
+        <v-btn
+          class="mb-3"
+          color="secondary"
+          outlined
+          :key="signType"
+          @click="signType = !signType"
+          >{{ signType ? 'Sign Up' : 'Login' }}</v-btn
+        >
+      </div>
+    </transition>
   </v-col>
 </template>
 
@@ -125,4 +137,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.fade-slideX-enter-active,
+.fade-slideX-leave-active {
+  transition: all 0.5s ease;
+}
+
+.fade-slideX-enter,
+.fade-slideX-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
+}
+
 </style>
