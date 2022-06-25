af6b037ca449e22d52202b022ec40e9d64734e04
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2022-06-25 15:43:34 +0700

    animation on show element

--- pages/myData.vue
+++ pages/myData.vue
@@ -1,7 +1,7 @@
 <template>
   <v-row justify="center">
     <theNavBar />
-
+    
     <v-card min-width="90%" v-if="foldersName.length > 0">
       <v-card-title style="padding: 6px 6px 6px 16px;">
         {{ userAuth ? `Your datas ${userAuth.displayName}` : 'My datas' }}
@@ -11,27 +11,31 @@
         <v-icon size="30px" class="mx-4" color="secondary" @click="showNewField(false)"
           >mdi-folder-plus-outline</v-icon
         >
-        <v-form ref="form" v-model="validForm" lazy-validation v-if="newFolder.create || newFolder.oldName">
-          <v-text-field
-            v-model="newFolder.name"
-            dense
-            :rules="nameRules"
-            :counter="10"
-            :label=" newFolder.oldName ? 'Rename Project' : 'Project Name'"
-            required
-          ></v-text-field>
-        </v-form>
-        <v-icon
-          v-if="newFolder.create || newFolder.oldName"
-          size="30px"
-          class="mx-4"
-          color="primary"
-          @click="editFolder"
-          >mdi-checkbox-marked-circle-outline</v-icon
-        >
+        <transition name="fade-slideX">
+          <v-form ref="form" v-model="validForm" lazy-validation v-if="newFolder.create || newFolder.oldName">
+            <v-text-field
+              v-model="newFolder.name"
+              dense
+              :rules="nameRules"
+              :counter="10"
+              :label=" newFolder.oldName ? 'Rename Project' : 'New Project Name'"
+              required
+            ></v-text-field>
+          </v-form>
+        </transition>
+        <transition name="fade-slide-rotate">
+          <v-icon
+            v-if="newFolder.create || newFolder.oldName"
+            size="30px"
+            class="mx-4"
+            color="primary"
+            @click="editFolder"
+            >mdi-checkbox-marked-circle-outline</v-icon
+          >
+        </transition>
+        
       </v-card-subtitle>
-      <v-card-actions class="d-flex flex-wrap justify-space-around" style="padding: 4px 4px 4px 8px;">
-        <!-- <span class="btn-active-folder">Select project to display :</span> -->
+      <v-card-actions class="d-flex flex-wrap justify-space-around  btn-active-folder" style="padding: 4px 8px 4px 8px;">
         <v-menu
           offset-y
           v-for="(name, l) in foldersName"
@@ -48,13 +52,15 @@
               v-on="on"
               @click="folderTooltip(name)"
             >
-              <v-icon left>
-                {{
-                  name === workOn
-                    ? 'mdi-folder-open-outline'
-                    : 'mdi-folder-outline'
-                }}
-              </v-icon>
+              <transition name="fade-rotate" mode="out-in">
+                <v-icon left :key="workOn">
+                  {{
+                    name === workOn
+                      ? 'mdi-folder-open-outline'
+                      : 'mdi-folder-outline'
+                  }}
+                </v-icon>
+              </transition>
               {{ name }}
             </v-btn>
           </template>
@@ -77,16 +83,21 @@
         <v-tooltip
           v-model="showTooltip"
           attach=".btn-active-folder"
-          nudge-bottom="100"
+          nudge-bottom="150"
+          nudge-right="150"
         >
           <span>This folder is already open !</span>
         </v-tooltip>
       </v-card-actions>
     </v-card>
+
     <v-col cols="11" class="text-center" v-if="!userAuth">
       <p>Remember to login or register to save your data !</p>
     </v-col>
-    <tableGeoJson :allDatas="GeoJsonTable" />
+    <transition name="fade-slideY" mode="out-in">
+      <tableGeoJson :allDatas="GeoJsonTable" :key="workOn"/>
+    </transition>
+    
   </v-row>
 </template>
 
@@ -180,4 +191,45 @@ export default {
 </script>
 
 <style lang="scss">
+.fade-slideX-enter-active,
+.fade-slideX-leave-active {
+  transition: all 0.5s ease;
+}
+.fade-slideX-enter,
+.fade-slideX-leave-to {
+  opacity: 0;
+  transform: translateX(50px);
+}
+.fade-slideY-enter-active,
+.fade-slideY-leave-active {
+  transition: all 0.3s ease;
+}
+.fade-slideY-enter,
+.fade-slideY-leave-to {
+  opacity: 0;
+  transform: translateY(40px);
+}
+
+.fade-slide-rotate-enter-active,
+.fade-slide-rotate-leave-active {
+  transition: all 0.5s ease;
+}
+
+.fade-slide-rotate-enter,
+.fade-slide-rotate-leave-to {
+  opacity: 0;
+  transform: translateX(50px) rotateZ(120deg);
+}
+
+.fade-rotate-enter-active,
+.fade-rotate-leave-active {
+  transition: all 1s ease;
+}
+
+.fade-rotate-enter,
+.fade-rotate-leave-to {
+  opacity: 0;
+  transform: rotateZ(90deg);
+}
+
 </style>
