ef1cb190caf065c1dea48c809780a351bd6711d5
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2022-06-28 17:08:47 +0700

    anim rotate icon

--- pages/gsap.vue
+++ pages/gsap.vue
@@ -4,6 +4,7 @@
       <v-btn @click="animate">gsap</v-btn>
       <v-btn @click="control(true)">resume</v-btn>
       <v-btn @click="control(false)">pause</v-btn>
+      <v-btn @click="controlStop()">stop</v-btn>
       <div class="circle"></div>
       <div class="circle"></div>
       <div class="circle"></div>
@@ -11,6 +12,9 @@
       <p class="textPlugin">xxxxxxxxxxxxxxxxxxxxxxx</p>
       <v-btn @click="textGsap">textGsap</v-btn>
       <v-btn @click="motionGsap">motionGsap</v-btn>
+      <v-icon color="primary" @click="testAnim" class="rotateIcon">mdi-map-marker</v-icon>
+      <v-icon color="secondary" @click="testAnim2" class="rotateIcon2">mdi-map-marker</v-icon>
+      <v-btn @click="stopOrigin">stopOrigin</v-btn>
     </v-col>
   </v-row>
 </template>
@@ -20,34 +24,45 @@ export default {
   data() {
     return {
       tl: undefined,
+      index: 1,
+      interval: undefined,
+      backToStart: false,
+      animeBtnLocation: undefined,
     }
   },
   methods: {
+    testAnim2() {
+      this.animeBtnLocation = gsap.to(".rotateIcon2", {duration: 1, rotateZ: 360, repeat: -1, yoyo: true});
+      
+      this.index = 0
+      this.interval = setInterval(() => {
+        this.index++
+        console.log(this.index);
+        if (this.index == 6) {
+          this.animeBtnLocation.pause();
+          this.animeBtnLocation.invalidate(); 
+          this.animeBtnLocation = gsap.to(".rotateIcon2", {duration: 1, rotateZ: 0, onComplete:function(){
+            console.log(this.targets()[0])
+          }});
+        }
+        if (this.index == 10) {
+          this.animeBtnLocation = gsap.to(".rotateIcon2", {duration: 1, scale: 0.8, repeat: -1, yoyo: true});
+        }
+        
+      }, 700); 
+    },
+    stopOrigin() {
+      this.animeBtnLocation.pause()
+      this.animeBtnLocation.invalidate();
+      gsap.to(".rotateIcon2", {duration: 1, scale: 1});
+      clearInterval(this.interval)
+    },
     animate() {
       this.tl.to('.circle', {
         x: 300,
-        duration: 4,
-        stagger: 0.5,
+        duration: 1,
         backgroundColor: 'blue',
         ease: 'back.out(1.7)',
-      })
-      this.tl.to('.motionPath', {
-        motionPath: {
-          path: [
-            { x: 100, y: 50 },
-            { x: 200, y: 0 },
-            { x: 300, y: 100 },
-          ],
-          alignOrigin: [0.5, 0.5],
-          autoRotate: true,
-          end: 1.5,
-        },
-        transformOrigin: '50% 50%',
-        duration: 5,
-        ease: 'power1.inOut',
-        onComplete: () => {
-          console.log('complete')
-        },
       })
     },
     control(e) {
@@ -69,26 +84,79 @@ export default {
           end: 1,
         },
         transformOrigin: '50% 50%',
-        duration: 5,
+        duration: 2,
         ease: 'power1.inOut',
-        repeat: 2,
+        repeat: 10,
         yoyo: true,
         onComplete: () => {
           console.log('complete')
         },
       })
     },
+    async controlStop(backToStart) {
+      this.backToStart = !this.backToStart
+      console.log(this.backToStart);
+      const stop = () => {
+        return new Promise((resolve, reject) => {
+          let repeats = Math.floor(this.tl.totalTime() / this.tl.duration());
+            if (repeats % 2 === 0) { 
+              repeats += 1;
+            }
+            this.tl.repeat(repeats)
+          clearInterval(this.interval)
+          resolve(true)
+          
+        });
+      }
+      let res = await stop()
+      if(res && this.backToStart) {
+        this.tl = gsap.timeline({ 
+          repeat: -1, 
+          yoyo: true 
+        })
+        this.tl.to('.rotateIcon', {
+            scale: 0.8,
+            duration: 1,
+            delay: 0.5,
+            ease: 'none',
+        })
+      }
+      
+    },
+    testAnim($event) {
+      this.tl = gsap.timeline({ 
+        repeat: -1, 
+        yoyo: true 
+      })
+      this.tl.to('.rotateIcon', {
+          rotateZ: 280,
+          duration: 1,
+          ease: 'none',
+      })
+      this.index = 0
+      this.interval = setInterval(() => {
+        this.index++
+        console.log(this.index);
+        if (this.index == 6) {
+          this.controlStop()     
+        }
+        
+      }, 700); 
+      
+      
+    }
   },
   mounted() {
     // if anim multiple element, timeline start when the other one stop
-    this.tl = gsap.timeline({ repeat: 2, yoyo: true })
+    /* this.tl = gsap.timeline({ repeat: -1, yoyo: true }) */
     // observer plugin on scroll or touch
     Observer.create({
       target: window,
       type: 'wheel,touch',
       onUp: () => this.control(false),
       onDown: () => this.control(true),
-    })
+    }) 
+
   },
 }
 </script>
@@ -105,5 +173,26 @@ export default {
   height: 20px;
   background-color: blue;
 }
-
-</style>
+@keyframes rotate {
+  from {
+    transform: rotateZ(0deg);
+  }
+  to {
+    transform: rotateZ(360deg);
+  }
+}
+@keyframes rotateBack {
+  from {
+    transform: rotateZ(180deg);
+  }
+  to {
+    transform: rotateZ(0deg);
+  }
+}
+/* .rotateIcon {
+  animation: rotate 1s 1s ease infinite both;
+}
+.backToOrigin {
+  animation: rotateBack 1s 1s ease forwards;
+}
+ */</style>
